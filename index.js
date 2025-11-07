3fb6a785492282a23d4b12a602f1498610c4d156
Pornput0012 <pornput.sood@mail.kmutt.ac.th>
2025-11-07 22:01:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+  return a + b;
+}
+
+console.log("✅ Hello GitHub Actions!");
+module.exports = sum;
---
+++ test.js
@@ -0,0 +1,9 @@
+const sum = require("./index");
+
+if (sum(2, 3) === 5) {
+  console.log("✅ Test passed");
+  process.exit(0);
+} else {
+  console.error("❌ Test failed");
+  process.exit(1);
+}
